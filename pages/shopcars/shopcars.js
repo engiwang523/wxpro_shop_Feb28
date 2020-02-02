ed70fd979965aebd6dcdc2255672026d66c56936
Jackie <1257868332@qq.com>
2020-02-02 18:33:39 +0800

    add cart page

--- pages/shopcars/shopcars.js
+++ pages/shopcars/shopcars.js
@@ -1,66 +1,124 @@
-// pages/shopcars/shopcars.js
+// page/component/new-pages/cart/cart.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    carts: [],               // 购物车列表
+    hasList: false,          // 列表是否有数据
+    totalPrice: 0,           // 总价，初始为0
+    selectAllStatus: false,    // 全选状态，默认全选
+    obj: {
+      name: "hello"
+    }
+  },
+  onShow() {
+    this.setData({
+      hasList: true,
+      carts: [
+        { id: 1, title: 'Jordan官方 AIR JORDAN XXXIV PF AJ34男子篮球鞋 BQ3381', image: '../../images/aj.png', num: 1, price: 1499, selected: false },
+        { id: 2, title: 'CONVERSE匡威官方 Chuck 70 经典高帮复古帆布鞋 情侣款 162053C', image: '../../images/converse.png', num: 1, price: 599, selected: false }
+      ]
+    });
+    this.getTotalPrice();
+  },
+  /**
+   * 当前商品选中事件
+   */
+  selectList(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    const selected = carts[index].selected;
+    carts[index].selected = !selected;
+    this.setData({
+      carts: carts
+    });
+    this.getTotalPrice();
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 删除购物车当前商品
    */
-  onLoad: function (options) {
-
+  deleteList(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    carts.splice(index, 1);
+    this.setData({
+      carts: carts
+    });
+    if (!carts.length) {
+      this.setData({
+        hasList: false
+      });
+    } else {
+      this.getTotalPrice();
+    }
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 购物车全选事件
    */
-  onReady: function () {
+  selectAll(e) {
+    let selectAllStatus = this.data.selectAllStatus;
+    selectAllStatus = !selectAllStatus;
+    let carts = this.data.carts;
 
+    for (let i = 0; i < carts.length; i++) {
+      carts[i].selected = selectAllStatus;
+    }
+    this.setData({
+      selectAllStatus: selectAllStatus,
+      carts: carts
+    });
+    this.getTotalPrice();
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * 绑定加数量事件
    */
-  onShow: function () {
-
+  addCount(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    num = num + 1;
+    carts[index].num = num;
+    this.setData({
+      carts: carts
+    });
+    this.getTotalPrice();
   },
 
   /**
-   * 生命周期函数--监听页面隐藏
+   * 绑定减数量事件
    */
-  onHide: function () {
-
+  minusCount(e) {
+    const index = e.currentTarget.dataset.index;
+    const obj = e.currentTarget.dataset.obj;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    if (num <= 1) {
+      return false;
+    }
+    num = num - 1;
+    carts[index].num = num;
+    this.setData({
+      carts: carts
+    });
+    this.getTotalPrice();
   },
 
   /**
-   * 生命周期函数--监听页面卸载
+   * 计算总价
    */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getTotalPrice() {
+    let carts = this.data.carts;                  // 获取购物车列表
+    let total = 0;
+    for (let i = 0; i < carts.length; i++) {         // 循环列表得到每个数据
+      if (carts[i].selected) {                     // 判断选中才会计算价格
+        total += carts[i].num * carts[i].price;   // 所有价格加起来
+      }
+    }
+    this.setData({                                // 最后赋值到data中渲染到页面
+      carts: carts,
+      totalPrice: total.toFixed(2)
+    });
   }
+
 })
